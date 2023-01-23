8a34252092765c2351f8520562f6463ddd09b46b
Deepak Maddheshiya <maddheshiya.deepak@gmail.com>
2023-01-23 23:54:47 +0530

    Removing App JS

--- App.js
+++
@@ -1,8 +0,0 @@
-const heading1 = React.createElement("h1", { id: "title1" }, "Namaste One !");
-const heading2 = React.createElement("h2", { id: "title2" }, "Namaste Two !");
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
