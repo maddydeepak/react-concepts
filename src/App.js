7b2f2f08428a2701e88ef89c0944ab009ed40e4b
Deepak Maddheshiya <maddheshiya.deepak@gmail.com>
2023-01-24 23:37:20 +0530

    Outlet Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Instamart from "./components/Instamart";
 import Error from "./components/Error";
@@ -21,7 +22,7 @@ const AppLayout = () => {
   return (
     <>
       <Header />
-      <Body />
+      <Outlet />
       <Footer />
     </>
   );
@@ -32,10 +33,20 @@ const appRouter = createBrowserRouter([
     path: "/",
     element: <AppLayout />,
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../components/assets/img/logo.png";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="header">
-      <a href="/">
+      <Link to="/">
         <img src={logo}></img>
-      </a>
+      </Link>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Cart</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
+        <Link to="/cart">
+          <li>Cart</li>
+        </Link>
       </ul>
       {isLoggedIn ? (
         <button onClick={() => setIsLoggedIn(false)}>Logout</button>
