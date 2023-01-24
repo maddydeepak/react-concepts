95d59426cefde5ac823259b64ade3b457dcbc9dc
Deepak Maddheshiya <maddheshiya.deepak@gmail.com>
2023-01-24 23:16:06 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Instamart from "./components/Instamart";
+import Error from "./components/Error";
 
 // const heading1 = React.createElement("h1", { id: "title1" }, "Namaste One !");
 // const heading2 = React.createElement("h2", { id: "title2" }, "Namaste Two !");
@@ -30,6 +31,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  return (
+    <>
+      <h1>Error: {err.status}</h1>
+      <h2>Error Details: {err.statusText}</h2>
+    </>
+  );
+};
+
+export default Error;
